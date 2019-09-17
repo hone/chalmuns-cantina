eca388c64259e852b30c26926f2a9bd780a9856b
Daniel Bright <brahyt@gmail.com>
2019-09-16 18:19:40 -0700

    fixed credits icon

--- app/helpers/icon-fill.js
+++ app/helpers/icon-fill.js
@@ -4,7 +4,7 @@ import { htmlSafe } from '@ember/string';
 
 export default helper(function iconFill([string]) {
   let iconString = string.replace(/([\d])k/g, function(_, credits) {
-    return `<i class="icon-credits"></i>${credits},000`;
+    return `<i class="icon-credits"></i> ${credits},000`;
   }).replace(/\(-\)/g, function() {
     return `<i class="icon-negative"></i>`;
   }).replace(/@/, "at").replace(/([A-Z][A-Z]+)/g, function(_, word) {
