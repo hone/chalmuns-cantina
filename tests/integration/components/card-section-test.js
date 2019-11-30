fced3f0e3a0a0932913ddada41ab0bf4e5c482c6
Terence Lee <hone02@gmail.com>
2019-11-30 04:12:13 -0600

    get the all the tests to pass

--- tests/integration/components/card-section-test.js
+++ tests/integration/components/card-section-test.js
@@ -1,26 +1,31 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { find, findAll, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | card-section', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.title = "Title!";
+    this.subtitle = "Subtitle!";
+    this.steps = ["foo", "bar", "baz"];
 
-    await render(hbs`<CardSection />`);
+    await render(hbs`<CardSection @title={{this.title}} @subtitle={{this.subtitle}} @steps={{this.steps}}/>`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(find('[data-test-title]').textContent.trim(), this.title);
+    assert.equal(find('[data-test-subtitle]').textContent.trim(), this.subtitle);
+    assert.equal(findAll('[data-test-step]').length, 3);
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <CardSection>
-        template block text
-      </CardSection>
-    `);
+  test("it doesn't render the subtitle", async function(assert) {
+    this.title = "Title!";
+    this.steps = ["foo", "bar", "baz"];
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await render(hbs`<CardSection @title={{this.title}} @steps={{this.steps}}/>`);
+
+    assert.equal(find('[data-test-title]').textContent.trim(), this.title);
+    assert.notOk(find('[data-test-subtitle]'));
+    assert.equal(findAll('[data-test-step]').length, 3);
   });
 });
